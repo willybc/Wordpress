0707c18a42919c582e80d9818d9e28bc9ccc8e27
Willy Bardales <willybc06@gmail.com>
2017-04-28 15:59:49 -0300

    First Commit

---
+++ themes/Portafolio/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title><?php bloginfo(title); ?></title>
+  </head>
+  <body>
+<h1>
+  <h1><?php bloginfo(name); ?></h1>
+  <ul>
+    <li><b>Descripcion: <?php bloginfo(description);  ?></b></li>
+    <li><?php bloginfo(url);                          ?></li>
+    <li><?php bloginfo(language);                     ?></li>
+    <li><?php bloginfo(stylesheet_url);               ?></li>
+    <li><?php bloginfo(stylesheet_directory);         ?></li>
+  </ul>
+
+</h1>
+  </body>
+</html>
